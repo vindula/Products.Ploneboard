053ff721a926e0400721c393b3edbfe89e066962
Maurício Sousa <mauriciosousa@liberiun.com>
2012-08-27 16:24:43 -0300

    Customização Inicial do Produto(Tradução, e botao de editar post)

---
+++ Products/Ploneboard/Extensions/Install.py
@@ -0,0 +1,15 @@
+from Products.CMFCore.utils import getToolByName
+
+
+def install(self, reinstall=False):
+    tool = getToolByName(self, "portal_setup")
+    tool.runAllImportStepsFromProfile(
+            "profile-Products.Ploneboard:ploneboard",
+            purge_old=False)
+
+
+def uninstall(self):
+    portal_setup = getToolByName(self, 'portal_setup')
+    portal_setup.runAllImportStepsFromProfile(
+        'profile-Products.Ploneboard:uninstall',
+        purge_old=False)
